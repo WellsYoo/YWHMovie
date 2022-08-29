fa602d8880a4a138f87fbe8429d1d7640c35f40a
Wells <862638933@qq.com>
2022-08-29 17:55:47 +0800

    【 Feature】项目初始化，引入网络框架

---
+++ YWHMovie/Shared/YWHMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  YWHMovieApp.swift
+//  Shared
+//
+//  Created by Wells on 2022/8/29.
+//
+
+import SwiftUI
+
+@main
+struct YWHMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
